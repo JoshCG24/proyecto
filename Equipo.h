16a650b96ccb00f8ae6138aa23dc338669920fc1
joshua <joshua.cortes.gutierrez@est.una.ac.cr>
2026-04-24 23:08:03 -0600

    proyecto

--- Equipo.h
+++ Equipo.h
@@ -16,12 +16,12 @@ private:
     int criticidad;
     double estado;
     int tiempo_inavtivo;
-    vector<Incidencia*> Incidencia ;
+    vector<Incidencia*> Incidencias ;
 
 
     public:
     void degradar();
-    void agregarIncidencia();
+    void agregarIncidencia( Incidencia* incidencia);
     void resolverIncidencia();
     double calcularPrioridad();
     void aplicarMantenimiento();
