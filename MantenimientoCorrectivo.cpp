776e451f39358230d407f4e444b3d49298904498
joshua <joshua.cortes.gutierrez@est.una.ac.cr>
2026-04-24 23:20:48 -0600

    Joshua

--- MantenimientoCorrectivo.cpp
+++ MantenimientoCorrectivo.cpp
@@ -4,7 +4,13 @@
 
 #include "MantenimientoCorrectivo.h"
 
+MantenimientoCorrectivo::MantenimientoCorrectivo(int reduccionIncidencias, double mejoraEstado) {
+    this->mejoraEstado = mejoraEstado;
+    this->reduccionIncidencias = reduccionIncidencias;
+}
+
 void MantenimientoCorrectivo::aplicar(Equipo &e) {
+
 }
 
 void MantenimientoCorrectivo::reducirIncidencias(Equipo &e) {
