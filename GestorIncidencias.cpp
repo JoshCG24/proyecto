853ac84ea1f3cecff9718bfbe9adfc8534dbd405
WorkcodeJor <jorvicbol30@gmail.com>
2026-04-27 21:12:25 -0600

    Jordy

--- GestorIncidencias.cpp
+++ GestorIncidencias.cpp
@@ -7,6 +7,9 @@
 
 void GestorIncidencias::generarIncidencia(Equipo *e, int dia) {
 
+    /*Los estados de los equipos como se manejaran*/
+    int probabilidad=0;
+    probabilidad =probabiblidad+e->getCriticidad();
 
 
 }
--- MantenimientoPreventivo.cpp
+++ MantenimientoPreventivo.cpp
@@ -5,4 +5,17 @@
 #include "MantenimientoPreventivo.h"
 
 void MantenimientoPreventivo::aplicar(Equipo &e) {
+
+    double mejora = 0.15;
+
+    double nuevoEstado = e.getEstado() + mejora;
+
+    if (nuevoEstado > 1.0) {
+        nuevoEstado = 1.0;
+    }
+
+    e.setEstado(nuevoEstado);
+    e.getTiempoInactivo();
+
+
 }
