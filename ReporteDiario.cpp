a38d4d6076c050daa437e3da53b95e2de0a36f11
joshua <joshua.cortes.gutierrez@est.una.ac.cr>
2026-04-30 18:37:22 -0600

    Joshua ordenar termiando

--- ReporteDiario.cpp
+++ ReporteDiario.cpp
@@ -5,4 +5,26 @@
 #include "ReporteDiario.h"
 
 string ReporteDiario::generarTexto() {
+    string reporte = "";
+
+    // dia de accion
+    reporte += "Dia " + to_string(id) + "\n";
+
+    // los 3 equipos que mas prioridad
+    reporte += "Asignados: ";
+    for (int i = 0; i < equiposAtendidos.size(); i++) {
+        reporte += equiposAtendidos[i]->getId() + " ";
+    }
+    reporte += "\n";
+
+    // Mostrar faltantes
+    reporte += "Backlog pendiente: " + to_string(equiposPendientes.size()) + "\n";
+
+    // Datos del estado del laboratori
+    reporte += "Riesgo global: " + to_string(riesgoTotal) + "\n";
+    reporte += "Estado general: " + estadoGeneral + "\n";
+    reporte += "----------------------------------\n";
+
+    return reporte;
+
 }
